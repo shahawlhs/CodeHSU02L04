2fcfd014e201524fb9acc8a8e6d3b8f079713b6f
shahawlhs <shaha@wlhs.wlwv.k12.or.us>
2022-10-04 09:08:45 -0700

    Update RectangleTester.java

--- RectangleTester.java
+++ RectangleTester.java
@@ -10,12 +10,16 @@ public class RectangleTester
         
         //Make a rectangle called r1 with a width of 10 and height of 2 then print it
         
-         
+        Rectangle r1 = new Rectangle(10,2);  
+        System.out.println(r1);
          //print just the area of the rectangle
-         
+         r1.printArea();
          
          // Rectangle is the class and also the type of this object.
          // Make a new rectangle called r2 with a width of 5 and height of 15, print it, then print just its area
          
+         Rectangle r2 = new Rectangle(5,15);
+         System.out.println(r2);
+         r2.printArea();
     }
 }
